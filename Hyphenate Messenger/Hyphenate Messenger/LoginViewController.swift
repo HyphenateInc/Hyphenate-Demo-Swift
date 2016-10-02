5b6f7141055739d8382b782a50925d4ebf2edece
peng wan <peng.wan@opensilo.co>
2016-10-02 10:12:34 -0700

    Optimize login and registration flow

--- Hyphenate Messenger/Hyphenate Messenger/LoginViewController.swift
+++ Hyphenate Messenger/Hyphenate Messenger/LoginViewController.swift
@@ -11,23 +11,32 @@ import HyphenateFullSDK
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var userNameTextField: HyphenateTextField!
     @IBOutlet weak var passwordTextField: HyphenateTextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityIndicator.hidesWhenStopped = true
     }
     
     @IBAction func loginAction(sender: AnyObject) {
+        
+        activityIndicator.startAnimating()
+        
         EMClient.sharedClient().loginWithUsername(userNameTextField.text, password: passwordTextField.text) { (userName : String?, error : EMError?) in
-            if ((error) != nil) {
-                print("error is \(error?.description)")
-            }
-            print("is login \(EMClient.sharedClient().isLoggedIn)")
             
-            let mainVC = MainViewController()
-            HyphenateMessengerHelper.sharedInstance.mainVC = mainVC
-            self.navigationController?.pushViewController(mainVC, animated: true)
+            self.activityIndicator.stopAnimating()
+            
+            if ((error) != nil) {
+                let alert = UIAlertController(title:"Login Failure", message: error?.errorDescription, preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("ok", comment: "ok"), style: .Cancel, handler: nil))
+                UIApplication.sharedApplication().keyWindow?.rootViewController?.presentViewController(alert, animated: true, completion: nil)
+            } else if EMClient.sharedClient().isLoggedIn {
+                let mainVC = MainViewController()
+                HyphenateMessengerHelper.sharedInstance.mainVC = mainVC
+                self.navigationController?.pushViewController(mainVC, animated: true)
+            }
         }
     }
 }
--- Hyphenate Messenger/Hyphenate Messenger/SignupViewController.swift
+++ Hyphenate Messenger/Hyphenate Messenger/SignupViewController.swift
@@ -11,13 +11,38 @@ import HyphenateFullSDK
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextfield: HyphenateTextField!
+    @IBOutlet weak var passwordTextfield: HyphenateTextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityIndicator.hidesWhenStopped = true
     }
 
     @IBAction func signupAction(sender: AnyObject) {
-        EMClient.sharedClient().registerWithUsername("pengpeng5", password: "password") { (userID, error) in
+        activityIndicator.startAnimating()
+        EMClient.sharedClient().registerWithUsername(usernameTextfield.text, password: passwordTextfield.text) { (userID, error) in
             
+            if ((error) != nil) {
+                self.activityIndicator.stopAnimating()
+                let alert = UIAlertController(title:"Registration Failure", message: error?.errorDescription, preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("ok", comment: "ok"), style: .Cancel, handler: nil))
+                UIApplication.sharedApplication().keyWindow?.rootViewController?.presentViewController(alert, animated: true, completion: nil)
+            } else {
+                EMClient.sharedClient().loginWithUsername(self.usernameTextfield.text, password: self.passwordTextfield.text, completion: { (userID, error) in
+                    if ((error) != nil) {
+                        let alert = UIAlertController(title:"Login Failure", message: error?.errorDescription, preferredStyle: .Alert)
+                        alert.addAction(UIAlertAction(title: NSLocalizedString("ok", comment: "ok"), style: .Cancel, handler: nil))
+                        UIApplication.sharedApplication().keyWindow?.rootViewController?.presentViewController(alert, animated: true, completion: nil)
+                    } else if EMClient.sharedClient().isLoggedIn {
+                        let mainVC = MainViewController()
+                        HyphenateMessengerHelper.sharedInstance.mainVC = mainVC
+                        self.navigationController?.pushViewController(mainVC, animated: true)
+                    }
+                })
+            }
         }
     }
 }
